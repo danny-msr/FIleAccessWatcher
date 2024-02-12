fdc9a15d1c71844806badf41c073dddef6b6d165
D.Messer <scarecrow.kyouiti+github@gmail.com>
2024-02-13 01:24:54 +0900

    サンプルできた

---
+++ FileAccessWatcher/FileAccessWatcher/FileAccessWatcherViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileAccessWatcher
+{
+	class AccessLogItem
+	{
+		public string? Name { get; set; }
+		public DateTime TimeStamp { get; set; }
+		public WatcherChangeTypes watcherChangeTypes { get; set; }
+	}
+
+	class FileAccessWatcherViewModel : INotifyPropertyChanged
+	{
+		private ObservableCollection<AccessLogItem> accessLogItems = new ObservableCollection<AccessLogItem>();
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public ObservableCollection<AccessLogItem> AccessLogItems
+		{
+			get => accessLogItems; set {
+				SetProperty(ref accessLogItems, value);
+			}
+		}
+
+		public void SetProperty<T>(ref T target, T value, [CallerMemberName] string caller = "")
+		{
+			target = value;
+
+			if (PropertyChanged == null)
+				return;
+			PropertyChangedEventArgs arg = new PropertyChangedEventArgs(caller);
+			PropertyChanged.Invoke(this, arg);
+		}
+
+		public FileAccessWatcherViewModel(){
+			this.accessLogItems.Add(new AccessLogItem() {
+			Name = "てｓｔ"});
+		}
+
+	}
+}
